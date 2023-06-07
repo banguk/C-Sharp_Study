aad35845ee5a757ad7fa981b93c0f81a223e0715
우상욱 <woosanguk@usang-ug-ui-MacBookPro.local>
2023-06-07 23:20:11 +0900

    method Example

---
+++ src/Method_1/Method_1/BasicMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Method_1
+{
+    public class BasicMethod
+    {
+        public BasicMethod()
+        {
+        }
+
+        // 값 매개변수 메서드 예제
+        public string Combine(string _driveLetter, string _folderPath, string _fileName)
+        {
+            string path;
+            path = string.Format("{1}{0}{2}{0}{3}", System.IO.Path.DirectorySeparatorChar, _driveLetter, _folderPath, _fileName);
+            return path;
+        }
+    }
+}
+
